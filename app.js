bf396a111f280410d7a4243f76fbd4301d79eb21
obregonpablo <obregonpablo2022@gmail.com>
2023-10-09 22:54:28 -0300

    vista casi completa

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () =>{
+    console.log("Servidor corriendo en el puerto 3000")
+})
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname,'views/home.html'))
+})
+
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname,'views/register.html'))
+})
+
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname,'views/login.html'))
+})
 
